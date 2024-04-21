b65e24d63bd20118894f0accf5ef450a6e249db8
willyanr <willyan.voyage@gmail.com>
2024-04-21 00:13:36 -0400

    update orders component

--- frontend/src/app/components/orders/orders.component.ts
+++ frontend/src/app/components/orders/orders.component.ts
@@ -1,7 +1,13 @@
+import { threadId } from 'worker_threads';
+import { modelFinancial, modelMenu, modelTransations } from './../../models/financial.model';
 import { OrdertableComponent } from './../ordertable/ordertable.component';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FinancialService } from '../../service/financial.service';
+import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { response } from 'express';
 
 @Component({
   selector: 'app-orders',
@@ -9,6 +15,7 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
   imports: [CommonModule,
     OrdertableComponent,
     SidebarComponent,
+    FormsModule,
 
 
 
@@ -17,5 +24,77 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
   styleUrl: './orders.component.css'
 })
 export class OrdersComponent {
+  menu$ = new Observable <modelMenu[]>;
+  data$ = new Observable <modelTransations[]>;
+  sales$ = new Observable <modelFinancial[]>;
+  productActive: modelMenu[] = [];
+  productDeactivate: modelMenu[] = [];
+  totalMenu: number = 0;
+  totalSales: number = 0;
+
+
+
+  constructor(private FinancialService: FinancialService){}
+
+  ngOnInit(): void {
+    this.getMenu();
+    this.getTransations();
+    this.getSales();
+    
+  }
+
+  getMenu() {
+    this.menu$ = this.FinancialService.obterMenu();
+    this.menu$.subscribe(response => {
+      response.forEach(item => {
+        this.totalMenu += 1;
+        if(item.status){
+          this.productActive.push(item)
+        }
+        else{
+          this.productDeactivate.push(item)
+        }
+      });
+    });
+  }
+
+
+  getTransations(){
+    this.data$ = this.FinancialService.getTransations();
+    this.data$.subscribe(response => {
+      response.forEach(item => {
+        
+
+
+      })
+
+
+
+    })
+
+
+
+  }
+
+  getSales(){
+    this.sales$ = this.FinancialService.getSales();
+    this.sales$.subscribe(response => {
+      response.forEach(item => {
+        this.totalSales += 1;
+        
+
+
+      })
+
+
+
+    })
+
+
+
+  }
+  
+
+
 
 }
